3dccbee1c6f010a80477e3792197a986eca1f71c
Maciek Makowski <maciek.makowski@gmail.com>
2012-05-10 20:53:45 +0100

    factored out the part responsible for running client

--- src/test/scala/clientShutdown/Specification.scala
+++ src/test/scala/clientShutdown/Specification.scala
@@ -3,18 +3,38 @@ package clientShutdown
 import org.junit.runner.RunWith
 import org.specs2.runner.JUnitRunner
 
+object testClient extends App {
+  import com.bimbr.clisson.client.RecorderFactory
+  val record = RecorderFactory.getRecorder
+  println("Hey!")
+  // TODO: record event
+}
+
+abstract class ClissonIntegrationSpecification(client: App) extends org.specs2.Specification {
+  def runClient(): Int = {
+    import scala.sys.process._
+    val sep      = System.getProperty("file.separator")
+    val javaPath = System.getProperty("java.home") + sep + "bin" + sep + "java"
+    val command  = javaPath + " -cp " + classpath + " " + client.getClass.getCanonicalName.dropRight(1)
+    println(command)
+    val proc     = command.run()
+    proc.exitValue
+  }
+
+  def classpath = System.getProperty(classpathProperty)
+  
+  def classpathProperty = if (runningInSbt) "sbt.test.class.path" 
+                          else "java.class.path"
+    
+  def runningInSbt = System.getProperty("java.class.path") contains "sbt-launcher"
+}  
+
 @RunWith(classOf[JUnitRunner])
-class Specification extends org.specs2.Specification { def is =
+class Specification extends ClissonIntegrationSpecification(testClient) { def is =
   "when the client shuts down, remaining enqueued events are sent and the server connection is closed gracefully" ! clientShutdownTest
 
   def clientShutdownTest = {
-    import scala.sys.process._
-    val sep        = System.getProperty("file.separator")
-    val path       = System.getProperty("java.home") + sep + "bin" + sep + "java"
-    val command    = path + " -cp " + classpath + " " + client.getClass.getCanonicalName.dropRight(1)
-    println(command)
-    val proc = command.run()
-    println(proc.exitValue)
+    runClient()
     todo
   } 
 
@@ -25,17 +45,5 @@ class Specification extends org.specs2.Specification { def is =
 //verify log
 //shutdown the server
 
-  def classpath = System.getProperty(classpathProperty)
-  
-  def classpathProperty = if (runningInSbt) "sbt.test.class.path" else "java.class.path"
-    
-  def runningInSbt = System.getProperty("java.class.path") contains "sbt-launcher"
-  
 }
 
-object client extends App {
-  import com.bimbr.clisson.client.RecorderFactory
-  val record = RecorderFactory.getRecorder
-  println("Hey!")
-  // TODO: record event
-}
